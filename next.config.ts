3f7713a1b5c4a3a642824c282566e33252ff4f16
Kunal Jain <jainkunal209@gmail.com>
2025-01-19 20:49:22 +0530

    chore: add rewrite for life in flashcards

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/life-in-flashcards",
+        destination: "https://life-in-flashcards.kunaljain.space",
+      },
+      {
+        source: "/life-in-flashcards/",
+        destination: "https://life-in-flashcards.kunaljain.space/",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
